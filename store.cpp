90c8ea3afba5b66b7b053e670c71361991c3a7cd
Jonathan Walzer <walzejon@uw.edu>
2020-11-29 22:32:15 -0800

    store and Hashtable

--- store.cpp
+++ store.cpp
@@ -24,7 +24,7 @@ void Store::addCustomers(ifstream &infile)
         infile >> ID;
         infile >> firstName;
         infile >> lastName;
-        //HashTable.add(ID,Customer(fistName,lastName);    - should i parse files here or let customer constructor do that?
+        //HashTable.insertItem(ID,Customer(ID,fistName,lastName));
     }
 }
 
@@ -33,6 +33,9 @@ void Store::addMovieInventory(ifstream &infile)
     //-	For comedy movies: F, Stock, Director, Title, Year it released
     //-	For drama movies: D, Stock, Director, Title, Year it released
     //-	For classics movies: C, Stock, Director, Title, Major actor Release date
+    char const comedy = 'F';
+    char const classic = 'C';
+    char const drama = 'D';
     for(;;)
     {
         if(infile.eof()) break;
@@ -41,6 +44,37 @@ void Store::addMovieInventory(ifstream &infile)
         //if(we have no tree for genre) skip whole line and keep going OR add a new binTree with this genre?
         //store genre trees in an array? so for(binTree A in arrray) {
         // if(genre == A) {add this movie to that binTree}
+    }
+}
+
+//Store
+void Store::printHelp(Node* root)
+{
+    if(root != nullptr)
+    {
+        cout<<root<<endl;
+        printHelp(root->left);
+        printHelp(root->right);
+    }
+}
+
+//preorder print
+void Store::printMovies(BinTree* A)
+{
+    printHelp(A->root);
+}
+
+void Store::doCommands(ifstream &infile)
+{
+    for(;;)
+    {
+        if(infile.eof()) break;
+        char command;
+        infile >> command;
+        if (command == 'I') {}//do I maybe commandI(infile) do rest of parsing there
+        if (command == 'B') {} //do B
+        if (command == 'H') {} //do H
+        if (command == 'R') {} // do R
     }
 }
 
