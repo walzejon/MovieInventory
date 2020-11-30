65f91c74856e84af3aea04fff9a03209fecc0c19
Jibran <ji6rans@gmail.com>
2020-11-29 22:33:18 -0800

    Added relational and comparison operators

--- classic.cpp
+++ classic.cpp
@@ -14,3 +14,56 @@ string Classic::getMARD() {
 void Classic::setMARD(string mard) {
 	this->MARD = mard;
 }
+
+bool Classic::operator<(const Classic& rightSide) const {
+	int maxTitle = max(this->title.length(), rightSide.title.length());
+	int maxDirector = max(this->director.length(), rightSide.director.length());
+	int maxMARD = max(this->MARD.length(), rightSide.MARD.length());
+	for (int i = 0; i < maxDirector; ++i) {
+		if (this->director[i] == rightSide.director[i] || this->director[i] <
+			rightSide.director[i]) continue;
+		else return false;
+	}
+	for (int i = 0; i < maxTitle; ++i) {
+		if (this->title[i] == rightSide.title[i] || this->title[i] < rightSide.title[i])
+			continue;
+		else return false;
+	}
+	for (int i = 0; i < maxMARD; ++i) {
+		if (this->MARD[i] == rightSide.MARD[i] || this->MARD[i] < rightSide.MARD[i])
+			continue;
+		else return false;
+	}
+	return true;
+}
+
+bool Classic::operator>(const Classic& rightSide) const {
+	int maxTitle = max(this->title.length(), rightSide.title.length());
+	int maxDirector = max(this->director.length(), rightSide.director.length());
+	int maxMARD = max(this->MARD.length(), rightSide.MARD.length());
+	for (int i = 0; i < maxDirector; ++i) {
+		if (this->director[i] == rightSide.director[i] || this->director[i] >
+			rightSide.director[i]) continue;
+		else return false;
+	}
+	for (int i = 0; i < maxTitle; ++i) {
+		if (this->title[i] == rightSide.title[i] || this->title[i] > rightSide.title[i])
+			continue;
+		else return false;
+	}
+	for (int i = 0; i < maxMARD; ++i) {
+		if (this->MARD[i] == rightSide.MARD[i] || this->MARD[i] > rightSide.MARD[i])
+			continue;
+		else return false;
+	}
+	return true;
+}
+
+bool Classic::operator==(const Classic& rightSide) const {
+	return (this->director == rightSide.director && this->title == rightSide.title &&
+		this->MARD == rightSide.MARD);
+}
+
+bool Classic::operator!=(const Classic& rightSide) const {
+	return !(*this == rightSide);
+}
