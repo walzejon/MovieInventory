c48597fada11bed111025140fed6943b99f26585
NoemiaGal <cindy.acevedo@icloud.com>
2020-12-07 23:31:52 -0800

    driver

---
+++ driver.cpp
@@ -0,0 +1,22 @@
+//driver
+
+#include <iostream>
+#include <fstream>
+
+#include "store.h"
+
+using namespace std;
+
+int main() {
+ 
+Store a;
+
+//ifstream customerFile("data4customer.txt");
+
+//a.readCommands(doCommands); //readFile
+
+a.doCommands();
+
+return 0;
+
+}
