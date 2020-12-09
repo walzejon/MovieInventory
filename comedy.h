802f8078b13b34e19908dc6ed096cc1b14706d3c
NoemiaGal <cindy.acevedo@icloud.com>
2020-12-08 23:35:55 -0800

    comments

--- comedy.h
+++ comedy.h
@@ -9,14 +9,21 @@
 class Comedy : public Movie {
     friend ostream& operator<<(ostream& outputStream, Comedy& com);
 public:
+    //constructor
     Comedy();
+    //copy constructor
     Comedy(string direc, string tit, int yearR);
+    //getter
     int getYearReleased() const;
+    //setter
     void setYearReleased(int year);
+    //display
     void display();
+    //returns true if movie is comedy or false if it's another genre
     virtual bool isClassic() const;
     virtual bool isDrama() const;
     virtual bool isComedy() const;
+    //comparison operators
     virtual bool operator<(const Movie& rightSide) const;
     virtual bool operator>(const Movie& rightSide) const;
     virtual bool operator==(const Movie& rightSide) const;
